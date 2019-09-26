35f266c94132f04d33663cdac7138fc2e8fe592a
Garrett Kelly <gckelly761@gmail.com>
2019-09-25 23:40:21 -0400

    Added self destruct and cleaned up form.

--- AutoclearInstaller/Form1.Designer.cs
+++ AutoclearInstaller/Form1.Designer.cs
@@ -28,13 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.outputBox = new System.Windows.Forms.RichTextBox();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.agreeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // outputBox
+            // 
+            this.outputBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outputBox.Location = new System.Drawing.Point(0, 0);
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.outputBox.Size = new System.Drawing.Size(530, 600);
+            this.outputBox.TabIndex = 0;
+            this.outputBox.Text = "";
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(12, 606);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 35);
+            this.cancelButton.TabIndex = 1;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // agreeButton
+            // 
+            this.agreeButton.Location = new System.Drawing.Point(445, 606);
+            this.agreeButton.Name = "agreeButton";
+            this.agreeButton.Size = new System.Drawing.Size(75, 35);
+            this.agreeButton.TabIndex = 2;
+            this.agreeButton.Text = "Agree";
+            this.agreeButton.UseVisualStyleBackColor = true;
+            this.agreeButton.Click += new System.EventHandler(this.agreeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(532, 653);
+            this.Controls.Add(this.agreeButton);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.outputBox);
+            this.Name = "Form1";
+            this.Text = "AutoClear";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox outputBox;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button agreeButton;
     }
 }
 
--- AutoclearInstaller/Form1.cs
+++ AutoclearInstaller/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace AutoclearInstaller
 {
@@ -15,6 +17,44 @@ namespace AutoclearInstaller
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void agreeButton_Click(object sender, EventArgs e)
+        {
+            createBat();
+            selfDestruct();
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            outputBox.AppendText("Thank you for downloading AutoClear, by Malechus!\n\nThis program will create folders in your Pictures and Downloads directories called 'Temp.' These folders can be used to store any files (like this installer) that you save or download to use once, but will not need again. Every time you log in, these folders will have been emptied, to keep those files from cluttering up your hard drive. In addition, every time you log in your Recycle Bin will have been emptied automatically.\n\n I recommend setting these temp folders as your default download and screenshot locations, you can do this through your browser settings and your screenshot application settings respectively. When you click agree below, the program will be installed and you will probably never see it again, as it runs very quickly and automatically. If you've changed your mind, click 'Cancel' to exit the installer.\n\n Enjoy!\n\n \u00a9 Garrett Kelly, 2019");
+        }
+
+        private void createBat()
+        {
+            StreamWriter autoclear = new StreamWriter(@"C:\Users\%username%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\autoclear.bat");
+            autoclear.WriteLine("echo off");
+            autoclear.WriteLine(@"rd /q /s c:\users\%username%\downloads\Temp");
+            autoclear.WriteLine(@"rd /q /s c:\users\%username%\pictures\Temp");
+            autoclear.WriteLine(@"rd /q /s %systemdrive%\$recycle.bin");
+            autoclear.WriteLine(@"mkdir c:\users\%username%\downloads\Temp");
+            autoclear.WriteLine(@"mkdir c:\users\%username%\pictures\Temp");
+            autoclear.WriteLine("exit");
+        }
+
+        private void selfDestruct()
+        {
+            Process selfDestruct = new Process();
+
+            selfDestruct.StartInfo.FileName = "cmd.exe";
+            selfDestruct.StartInfo.CreateNoWindow = true;
+            selfDestruct.StartInfo.Arguments = @"timeout 6 <nul & del /q " + Application.ExecutablePath;
         }
     }
 }
